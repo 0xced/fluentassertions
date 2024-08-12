fb5382ae14ef3d2e740d4d54bf6cc8383958193d
Cédric Luthi <cedric.luthi@gmail.com>
2024-08-12 14:28:52 +0200

    Add support for xUnit.net v3
    Fixes #2709

---
+++ Tests/TestFrameworks/XUnit3.Specs/FrameworkSpecs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace XUnit3.Specs;
+
+public class FrameworkSpecs
+{
+    [Fact]
+    public void When_xunit3_is_used_it_should_throw_xunit_exceptions_for_assertion_failures()
+    {
+        // Act
+        Action act = () => 0.Should().Be(1);
+
+        // Assert
+        Exception exception = act.Should().Throw<Exception>().Which;
+        exception.GetType().FullName.Should().Be("Xunit.Sdk.XunitException");
+    }
+}
